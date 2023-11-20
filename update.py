93614e781207e325963c90a0dabc35404bc181b8
Nick <32660358+dontcrash@users.noreply.github.com>
2023-11-20 17:14:24 +1030

    Add files via upload

--- update.py
+++ update.py
@@ -1,38 +1,40 @@
 import requests
 
-# File path for master list
-master_file = "nrd.txt"
+# URL to fetch the list of newly registered domains
+url = "https://dl.nrd-list.com/1/nrd-list-32-days.txt"
 
-# Function to update the master list with new domains
-def update_master_list():
-    global master_file  # Accessing the global variable
+# Function to extract TLD from a domain line
+def get_tld(domain):
+    components = domain.split('^')
+    if len(components) >= 1:
+        tld_component = components[0].strip("|").strip(".")
+        if tld_component.count('.') >= 1:
+            tld = tld_component.split('.')[-1]
+            return tld
+    return None
 
-    # URL to fetch the list of newly registered domains
-    url = "https://dl.nrd-list.com/1/nrd-list-32-days.txt"
-
+# Function to update the domain files with new domains
+def update_domain_files():
     # Download the list of newly registered domains
     response = requests.get(url)
     new_domains = response.text.splitlines()
 
-    # Filter new domains to those starting with '||'
-    new_domains = [domain for domain in new_domains if domain.startswith("||")]
+    # Filter new domains to those starting with '||' and extract TLDs
+    tld_domains = {}
+    for domain in new_domains:
+        if domain.startswith("||"):
+            tld = get_tld(domain)
+            if tld:
+                tld_domains.setdefault(tld, []).append(domain)
 
-    # Read the contents of the master file
-    try:
-        with open(master_file, 'r') as file:
-            master_domains = [line.strip() for line in file.readlines()]
-    except FileNotFoundError:
-        master_domains = []
+    # Save domains to separate files for each TLD
+    for tld, domains in tld_domains.items():
+        filename = f"lists/{tld}.txt"
+        with open(filename, 'a') as tld_file:
+            tld_file.write('\n'.join(domains) + '\n')
+        print(f"Domains for TLD {tld} written to {filename}")
 
-    # Combine new and master domains, remove duplicates, and sort them alphabetically
-    combined_domains = sorted(set(new_domains + master_domains), key=lambda x: (x.startswith("||"), x))
-    combined_domains = [line.strip() for line in combined_domains if line.startswith("||")]
-
-    # Save the sorted unique domains to the master file
-    with open(master_file, 'w') as file:
-        file.write("\n".join(combined_domains))
-
-    print("Master list updated successfully!")
+    print("Domain files updated successfully!")
 
 if __name__ == "__main__":
-    update_master_list()
+    update_domain_files()
